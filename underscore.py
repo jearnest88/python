38ba1912e3fb14f1f2ed49e355e0ecbd08c6041b
Jeff Earnest <jearnest88@gmail.com>
2016-06-20 00:42:03 -0400

    started underscore

--- underscore.py
+++ underscore.py
@@ -1,14 +1,45 @@
 class Underscore(object):
-    def map(self):
+    def map(self, arr, callback):
+        arrnew = []
+        for data in arr:  #cycles through the numbers in the original array
+            arrnew.append(callback(data))  #performs the lambda method on number and adds it to the new array
+        return arrnew
 
-    def reduce(self):
+    # def reduce(self):
 
-    def find(self):
+    def find(self, arr, callback):#adds qualifying numbers to the new array
+        arrnew = []
+        for data in arr: #cycles through array
+            if callback(data) == True: #checks if number meets lamdba
+                arrnew.append(data) #adds qualifying numbers to the new array
+        return arrnew
 
-    def filter(self):
+    def filter(self, arr, callback):
+        arrnew = []
+        for data in arr:  #same as find
+            if callback(data) == True:
+                arrnew.append(data)
+        return arrnew
 
-    def reject(self):
+    def reject(self, arr, callback): #this is just the opposite of filter
+        arrnew = []
+        for data in arr:
+            if callback(data) == False:
+                arrnew.append(data)
+        return arrnew
 
 
 _ = Underscore()
-evens = _.filter([1,2,3,4,5,6], lambda x: x % 2 == 0)
+nums = [1,2,3,4,5,6]
+
+mapMeth = _.map(nums, lambda x: x * 2)
+print mapMeth
+
+findMeth = _.find(nums, lambda x: x % 2 == 0)
+print findMeth
+
+filterMeth = _.filter(nums, lambda x: x % 2 == 0)
+print filterMeth
+
+rejectMeth = _.reject(nums, lambda x: x % 2 == 0)
+print rejectMeth
