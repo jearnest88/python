cfd68e10b1e0379681a00df17b9866e7a28a73b4
Jeff Earnest <jearnest88@gmail.com>
2016-06-19 23:52:39 -0400

    added mathDojo

--- mathDojo.py
+++ mathDojo.py
@@ -1,12 +1,12 @@
 class mathDojo(object):
     def __init__(self):
-        print ('I am math')
+        print ('The answer is: ')
         self.sum = 0
     def add(self, *number):
         for arg in number:
             if(isinstance(arg, list) or isinstance(arg, tuple)):
                 for val in arg:
-                    self.sum = self.sum + arg
+                    self.sum = self.sum + val
             else:
                 self.sum = self.sum + arg
         return self
@@ -20,6 +20,6 @@ class mathDojo(object):
         return self
 
 math = mathDojo()
-math.add(2,2,3,4).add(56,9).subtract(3,6,8)
+math.add(8,2,3,[4]).add(56,9).subtract([3,6],8)
 
 print(math.sum)
---
+++ underscore.py
@@ -0,0 +1,14 @@
+class Underscore(object):
+    def map(self):
+
+    def reduce(self):
+
+    def find(self):
+
+    def filter(self):
+
+    def reject(self):
+
+
+_ = Underscore()
+evens = _.filter([1,2,3,4,5,6], lambda x: x % 2 == 0)
